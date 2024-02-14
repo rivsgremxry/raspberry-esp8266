d6213a1390b1a17cdd3f4ff2107bed2a6b0f2ccc
Vitaliy <antylameer29@gmail.com>
2024-02-14 13:30:21 +0200

    Dynamic determination of the number of sensors

--- flask_esp8266_app/app.py
+++ flask_esp8266_app/app.py
@@ -10,10 +10,13 @@ import pytz
 app = Flask(__name__)
 
 # Configure the database connection URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask_esp:flask_esp@localhost/flask_esp'
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://flask_esp:flask_esp@localhost/flask_esp"
+)
 
 # Create a SQLAlchemy database instance
 db = SQLAlchemy(app)
+
 
 # Define a database model for sensor data
 class SensorData(db.Model):
@@ -21,7 +24,10 @@ class SensorData(db.Model):
     name = db.Column(db.String(50))
     humidity = db.Column(db.Float)
     temperature = db.Column(db.Float)
-    timestamp = db.Column(db.TIMESTAMP, default=lambda: datetime.now(pytz.timezone('Europe/Riga')))
+    timestamp = db.Column(
+        db.TIMESTAMP, default=lambda: datetime.now(pytz.timezone("Europe/Riga"))
+    )
+
 
 # Define a function to clear all data in the SensorData table
 def clear_data():
@@ -30,46 +36,82 @@ def clear_data():
         SensorData.query.delete()
         db.session.commit()
 
+
 # Define the main route for rendering the index.html template
-@app.route('/')
+@app.route("/")
 def index():
-    # Retrieve the latest data for esp8266-1 and esp8266-2 sensors
-    latest_data_esp8266_1 = SensorData.query.filter_by(name='esp8266-1').order_by(SensorData.timestamp.desc()).first()
-    latest_data_esp8266_2 = SensorData.query.filter_by(name='esp8266-2').order_by(SensorData.timestamp.desc()).first()
+    # Get a list of unique sensor names from the database
+    sensor_names = db.session.query(SensorData.name).distinct().all()
 
-    # Check if data exists before accessing attributes
-    if latest_data_esp8266_1:
-        temperature_esp8266_1 = latest_data_esp8266_1.temperature
-        humidity_esp8266_1 = latest_data_esp8266_1.humidity
-    else:
-        temperature_esp8266_1 = None
-        humidity_esp8266_1 = None
+    # Initializing lists for storing temperature and humidity data
+    sensor_data_list = []
 
-    if latest_data_esp8266_2:
-        temperature_esp8266_2 = latest_data_esp8266_2.temperature
-        humidity_esp8266_2 = latest_data_esp8266_2.humidity
-    else:
-        temperature_esp8266_2 = None
-        humidity_esp8266_2 = None
+    # Process each sensor
+    for sensor_name in sensor_names:
+        # Get the latest data for each sensor
+        latest_data = (
+            SensorData.query.filter_by(name=sensor_name[0])
+            .order_by(SensorData.timestamp.desc())
+            .first()
+        )
 
-    # Calculate average current temperature and humidity for esp8266-1 and esp8266-2 sensors
-    avg_data_temperature = round(((temperature_esp8266_1 or 0) + (temperature_esp8266_2 or 0)) / 2, 1)
-    avg_data_humidity = round(((humidity_esp8266_1 or 0) + (humidity_esp8266_2 or 0)) / 2, 1)
+        # If the data is available, add it to the lists
+        if latest_data:
+            # Define a new name for each sensor
+            display_name = (
+                "Corridor"
+                if sensor_name[0] == "esp8266-1"
+                else "Bedroom" if sensor_name[0] == "esp8266-2" else sensor_name[0]
+            )
+
+            sensor_data_list.append(
+                {
+                    "name": display_name,
+                    "temperature": latest_data.temperature,
+                    "humidity": latest_data.humidity,
+                    "timestamp": (
+                        latest_data.timestamp.strftime("%H:%M")
+                        if latest_data.timestamp
+                        else "N/A"
+                    ),
+                }
+            )
+
+    # Calculate average values only for active sensors
+    avg_data_temperature = (
+        round(
+            sum(data["temperature"] for data in sensor_data_list)
+            / len(sensor_data_list),
+            1,
+        )
+        if sensor_data_list
+        else None
+    )
+    avg_data_humidity = (
+        round(
+            sum(data["humidity"] for data in sensor_data_list) / len(sensor_data_list),
+            1,
+        )
+        if sensor_data_list
+        else None
+    )
 
     # Render the index.html template with data
-    return render_template('index.html',
-                           latest_data_esp8266_1=latest_data_esp8266_1,
-                           latest_data_esp8266_2=latest_data_esp8266_2,
-                           avg_data_temperature=avg_data_temperature,
-                           avg_data_humidity=avg_data_humidity)
+    return render_template(
+        "index.html",
+        sensor_data_list=sensor_data_list,
+        avg_data_temperature=avg_data_temperature,
+        avg_data_humidity=avg_data_humidity,
+    )
+
 
 # Define a route to handle the addition of sensor data via POST requests
-@app.route('/add_data', methods=['POST'])
+@app.route("/add_data", methods=["POST"])
 def add_data():
     # Retrieve data from the POST request
-    name = request.form.get('name')
-    humidity = request.form.get('humidity')
-    temperature = request.form.get('temperature')
+    name = request.form.get("name")
+    humidity = request.form.get("humidity")
+    temperature = request.form.get("temperature")
 
     # Check if the received data is valid
     if name is not None and humidity is not None and temperature is not None:
@@ -84,8 +126,10 @@ def add_data():
         # Return an error response for invalid data
         return "Invalid data", 400
 
+
 # Schedule a daily task to clear data in the SensorData table at 00:00
 schedule.every().day.at("00:00").do(clear_data)
+
 
 # Define a function to run the scheduled tasks in a separate thread
 def run_schedule():
@@ -93,9 +137,11 @@ def run_schedule():
         schedule.run_pending()
         time.sleep(1)
 
+
 # Start the scheduled tasks in a separate thread
-if __name__ == '__main__':
+if __name__ == "__main__":
     from threading import Thread
+
     scheduler_thread = Thread(target=run_schedule)
     scheduler_thread.start()
 
@@ -104,7 +150,7 @@ if __name__ == '__main__':
         db.create_all()
 
     # Run the Flask application
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0")
 
     # Wait for the scheduler thread to finish
     scheduler_thread.join()
